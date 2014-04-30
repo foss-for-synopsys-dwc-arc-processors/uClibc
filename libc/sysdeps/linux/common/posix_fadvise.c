foss-for-synopsys-dwc-arc-processors/uClibc

commit ee84b8b40004c970ab0ac660cb04f12cc2748e84

linux: posix_fadvise: use new SYSCALL_ALIGN_64BIT
Now that we have a new SYSCALL_ALIGN_64BIT define for tracking the
64bit register shift behavior, use it.  This allows us to delete
duplicated arm/xtensa files, as well as drop a few arch ifdefs from
common code.

Signed-off-by: Mike Frysinger <vapier@gentoo.org>

libc/sysdeps/linux/common/posix_fadvise.c
@@ -22,7 +22,7 @@ int posix_fadvise(int fd, off_t offset, off_t len, int advice)
 # if __WORDSIZE == 64
 	ret = INTERNAL_SYSCALL(fadvise64, err, 4, fd, offset, len, advice);
 # else
-#  ifdef __powerpc__
+#  if defined(__UCLIBC_SYSCALL_ALIGN_64BIT__)
 	ret = INTERNAL_SYSCALL(fadvise64, err, 6, fd, /*unused*/0,
 #  else
 	ret = INTERNAL_SYSCALL(fadvise64, err, 5, fd,

libc/sysdeps/linux/arm/posix_fadvise.c
@@ -1,33 +1,0 @@
-/* vi: set sw=4 ts=4: */
-/*
- * posix_fadvise() for ARM uClibc
- * http://www.opengroup.org/onlinepubs/009695399/functions/posix_fadvise.html
- *
- * Copyright (C) 2000-2006 Erik Andersen <andersen@uclibc.org>
- *
- * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
- */
-#include <sys/syscall.h>
-
-#ifdef __NR_arm_fadvise64_64
-# include <fcntl.h>
-# include <endian.h>
-
-# define HIGH_BITS(x) (sizeof(x) > 4 ? (x) >> 32 : 0)
-
-/* This is for the ARM version of fadvise64_64 which swaps the params
- * about to avoid having ABI compat issues
- */
-#define __NR___syscall_arm_fadvise64_64 __NR_arm_fadvise64_64
-int posix_fadvise(int fd, off_t offset, off_t len, int advise)
-{
-  INTERNAL_SYSCALL_DECL (err);
-  int ret = INTERNAL_SYSCALL (arm_fadvise64_64, err, 6, fd, advise,
-                              __LONG_LONG_PAIR (HIGH_BITS(offset), (long)offset),
-                              __LONG_LONG_PAIR (HIGH_BITS(len), (long)len));
-
-    if (INTERNAL_SYSCALL_ERROR_P (ret, err))
-      return INTERNAL_SYSCALL_ERRNO (ret, err);
-    return 0;
-}
-#endif

libc/sysdeps/linux/xtensa/posix_fadvise.c
@@ -1,27 +1,0 @@
-/* vi: set sw=4 ts=4: */
-/*
- * posix_fadvise() for Xtensa uClibc
- *
- * Copyright (C) 2000-2006 Erik Andersen <andersen@uclibc.org>
- * Copyright (C) 2007 Tensilica Inc.
- *
- * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
- */
-
-#include <sys/syscall.h>
-
-#ifdef __NR_fadvise64_64
-# include <fcntl.h>
-# include <endian.h>
-
-int posix_fadvise (int fd, off_t offset, off_t len, int advice)
-{
-	INTERNAL_SYSCALL_DECL (err);
-	int ret = INTERNAL_SYSCALL (fadvise64_64, err, 6, fd, advice,
-				    __LONG_LONG_PAIR ((long) (offset >> 31), (long) offset),
-				    __LONG_LONG_PAIR ((long) (len >> 31), (long) len));
-	if (INTERNAL_SYSCALL_ERROR_P (ret, err))
-		return INTERNAL_SYSCALL_ERRNO (ret, err);
-	return 0;
-}
-#endif

libc/sysdeps/linux/arm/Makefile.arch
@@ -14,7 +14,6 @@ SSRC-y := \
 
 SSRC-$(UCLIBC_HAS_LFS) += mmap64.S
 SSRC-$(UCLIBC_HAS_THREADS_NATIVE) += libc-aeabi_read_tp.S libc-thumb_atomics.S
-CSRC-$(UCLIBC_HAS_ADVANCED_REALTIME) += posix_fadvise.c
 CSRC-$(UCLIBC_HAS_CONTEXT_FUNCS) += makecontext.c
 SSRC-$(UCLIBC_HAS_CONTEXT_FUNCS) += getcontext.S setcontext.S swapcontext.S
 

libc/sysdeps/linux/common/posix_fadvise64.c
@@ -23,7 +23,8 @@
 int posix_fadvise64(int fd, off64_t offset, off64_t len, int advice)
 {
 	INTERNAL_SYSCALL_DECL (err);
-# if defined __powerpc__ || defined __arm__ || defined __xtensa__
+	/* ARM has always been funky. */
+# if defined(__UCLIBC_SYSCALL_ALIGN_64BIT__) || defined(__arm__)
 	int ret = INTERNAL_SYSCALL (fadvise64_64, err, 6, fd, advice,
 			OFF64_HI_LO (offset), OFF64_HI_LO (len));
 # else

libc/sysdeps/linux/xtensa/Makefile.arch
@@ -9,5 +9,3 @@ CSRC-y := brk.c fork.c pread_write.c sigaction.c __syscall_error.c
 
 SSRC-y := bsd-_setjmp.S bsd-setjmp.S setjmp.S clone.S \
 	sigrestorer.S syscall.S mmap.S windowspill.S __longjmp.S vfork.S
-
-CSRC-$(UCLIBC_HAS_ADVANCED_REALTIME) += posix_fadvise.c

commit 8af9dc8f714d20a68df034f6ed1ededc2d752f5b

libc: posix_fadvise: Fix build breakage for !LFS
commit 00571b43df2e "libc: posix_fadvise: restore implementation for xtensa"
enabled posix_fadvise() for all arches (it was just not generated
before).

However this also unearthed an issue introduced by ee84b8b400
"linux: posix_fadvise: use new SYSCALL_ALIGN_64BIT" which is to
referencing LFS'ish code (off64_t) w/o proper checks which causes build
to break for !LFS.

Fix this by calling posix_fadvise64() only for LFS case and open-code
it's equivalent for !LFS.

Signed-off-by: Vineet Gupta <vgupta@synopsys.com>
Cc: Mike Frysinger <vapier@gentoo.org>
Cc: Baruch Siach <baruch@tkos.co.il>
Cc: Khem Raj <raj.khem@gmail.com>
Cc: markos Chandras <markos.chandras@gmail.com>
Cc: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

libc/sysdeps/linux/common/posix_fadvise.c
@@ -22,17 +22,34 @@
 # include <endian.h>
 # include <bits/wordsize.h>
 
-# ifdef __NR_fadvise64_64
+# if defined(__NR_fadvise64_64) && defined(__UCLIBC_HAS_LFS__)
+#include <_lfs_64.h>
+
 int posix_fadvise64(int fd, off64_t offset, off64_t len, int advice);
-# endif
+int posix_fadvise(int fd, off_t offset, off_t len, int advice)
+{
+	return posix_fadvise64(fd, offset, len, advice);
+}
+#else
 
 int posix_fadvise(int fd, off_t offset, off_t len, int advice)
 {
-# ifdef __NR_fadvise64_64
-	return posix_fadvise64(fd, offset, len, advice);
-# else
 	int ret;
 	INTERNAL_SYSCALL_DECL(err);
+
+# ifdef __NR_fadvise64_64
+#  if __WORDSIZE == 64
+	ret = INTERNAL_SYSCALL(fadvise64_64, err, 4, fd, offset, len, advice);
+#  else
+#   if defined(__UCLIBC_SYSCALL_ALIGN_64BIT__) || defined(__arm__)
+	ret = INTERNAL_SYSCALL(fadvise64_64, err, 6, fd, advice,
+			OFF_HI_LO (offset), OFF_HI_LO (len));
+#   else
+	ret = INTERNAL_SYSCALL(fadvise64_64, err, 6, fd,
+			OFF_HI_LO (offset), OFF_HI_LO (len), advice);
+#   endif
+#  endif
+# else  /* __NR_fadvise64 */
 #  if __WORDSIZE == 64
 	ret = INTERNAL_SYSCALL(fadvise64, err, 4, fd, offset, len, advice);
 #  else
@@ -43,12 +60,13 @@ int posix_fadvise(int fd, off_t offset, off_t len, int advice)
 #   endif
 			OFF_HI_LO (offset), len, advice);
 #  endif
+#  endif
 	if (INTERNAL_SYSCALL_ERROR_P (ret, err))
 		return INTERNAL_SYSCALL_ERRNO (ret, err);
 	return 0;
-#  endif
 }
 # if defined __UCLIBC_HAS_LFS__ && (!defined __NR_fadvise64_64 || __WORDSIZE == 64)
 strong_alias(posix_fadvise,posix_fadvise64)
 # endif
+#endif
 #endif